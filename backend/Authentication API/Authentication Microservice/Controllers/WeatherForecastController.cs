6ae159a7cf51df3cb2acf7ebb66448cf0f125cbe
somuhemu <69750169+somuhemu@users.noreply.github.com>
2022-07-15 20:51:25 +0530

    Add files via upload

---
+++ backend/Authentication API/Authentication Microservice/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AuthenticateMicroservices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Roles ="Customer")]
+    public class WeatherForecastController : ControllerBase
+    {
+        //readonly log4net.ILog _log4net;
+        /*public WeatherForecastController()
+        {
+            _log4net = log4net.LogManager.GetLogger(typeof(WeatherForecastController));
+        }*/
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            //_log4net.Info("Weather Forecast Invoked");
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ backend/Authentication API/Authentication Microservice/Repository/UserListRep.cs
@@ -0,0 +1,22 @@
+﻿using AuthenticateMicroservices.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AuthenticateMicroservices.Repository
+{
+    public class UserListRep : IUserListRep
+    {
+        public IEnumerable<User> getUserList()
+        {
+            var userList = new List<User>
+            {
+                new User{UserId=1,Password="1234",Roles="Employee"},
+                new User{UserId=2,Password="12345",Roles="Customer"}
+            };
+            return userList;
+        }
+    }
+}
